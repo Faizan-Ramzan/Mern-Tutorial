be24453e25e00e310cc2c85f0392c4108e0951f6
Faizan-Ramzan <36534453+Faizan-Ramzan@users.noreply.github.com>
2022-06-21 01:58:02 +0500

    Entering Message

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -2,8 +2,12 @@ const express = require('express')
 const router = express.Router()
 const { getGoals, setGoal, updateGoals, deleteGoal } = require('../controllers/goalControllers')
 
+//router.post('/', setGoal)
+//router.get('/', getGoal)
 router.route('/').get(getGoals).post(setGoal)
 
+//router.delete('/:id', deleteGoal)
+//router.update('/:id', updateGoal)
 router.route('/:id').put(updateGoals).delete(deleteGoal)
 
 module.exports = router
